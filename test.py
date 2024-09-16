2ba9f860314f6052504f5abf59d27230e0e98f9c
Thanyathon Tangsriprachit <65070105@kmitl.ac.th>
2024-09-16 22:40:04 +0700

    หน้าหลัก

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print('test')
+import io, base64
+from PIL import Image
+image = 
+# Assuming base64_str is the string value without 'data:image/jpeg;base64,'
+img = Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
+img.save('my-image.jpeg')
